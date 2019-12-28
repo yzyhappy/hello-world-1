e0def3848d73761a397827b37c122fabf8c3c231
yzyhappy <55172936+yzyhappy@users.noreply.github.com>
2019-12-28 22:54:08 +0800

    Add files via upload

--- SuperPrime.cpp
+++ SuperPrime.cpp
@@ -1,81 +1,78 @@
-class Number {
-public:
-  Number(int num):mNum(num) {
-  }
-  Number(const Number &n):mNum(n.mNum) {
-  }
-  ~Number();
-  NumberSet split() {
-  	NumberSet ns;
-  	int n = mNum;
-  	while(n != 0) {
-  	  int d = n % 10;
-  	  Number dig(d);
-  	  ns.add(dig);
-  	  n = n / 10;
-	}
-	return ns;
-  }
-  bool isPrime() {
-  	for(int i = 2; i < mNum; ++i) {
-  	  if(mNum % i == 0)
-  	    break;
-	}
-	if(i != mNum)
-	  return false;
-	return true;
-  }
-private:
-  const int mNum;
-};
-class NumberSet {
-private:
-  Number *nums[20];
-  int size;
-public:
-  NumberSet() {
-  	size = 0;
-  	for(int i = 0; i < 20; ++i)
-  	  nums[i] = NULL;
-  }
-  ~NumberSet() {
-  	for(int i = 0; i < size; ++i)
-  	  delete nums[i];
-  }
-  bool add(const Number &n) {
-  	if(size != 20 && nums[size] == NULL) {
-	 nums[size] = new Number(n); 
-     size += 1;
-     return true;
-    }
-    return false;
-  }
-  Number sum() {
-  	Number sum(0);
-  	for(int i = 0; i < size; ++i) {
-  	  if(nums[i] != NULL)
-  	    sum.add(*(nums[i]));
-	}
-	return sum;
-  }
-};
-class SuperPrime : public Number {
-public:
-  SuperPrime(int num);
-  ~SuperPrime();
+#include<stdio.h>
 
-  bool isPrime();
-
-  Prime sumBit();
-  Prime multiBit();
-  Prime sqaureSumBit();
-private:
-  const int num;
+int s (int p)
+{
+	int i;
+	int re=1;
+	if(p==2)
+	re=1;
+	else{
+		for(i=2;i<p;i++)
+		{
+			if(p%i==0)
+			{
+				re=0;
+				break;
+			}
+		}
+	}
+	return re;
 }
 int main()
 {
-    SuperPrime sp(113);
-    if(sp.isPrime()) {
-        ; // do something
-    }
-}
+	double result;
+	double time=0;
+	double final=0;
+	int i,d;
+	int num=0;
+	i=100;
+here:for(;i<10001;i++)
+	{
+		
+		if(s(i)==0)
+		{
+			i++;
+			goto here;
+		}
+		
+		else
+		{
+			
+			int rp=i;
+			while(rp>0)
+			{
+				rp=rp/10;
+				num++; 
+			}
+			rp=i;
+			int N[num];
+			for(d=0;d<num;d++)
+			{
+				N[d]=rp%10;
+				rp=rp/10;
+				if (rp<0)
+				break;
+			}
+			
+			int sum1=0,sum2=0;
+			for(d=0;d<num;d++)
+			{
+				sum1+=N[d]*N[d];
+				sum2+=N[d];
+			}
+		if(s(sum1)==0||s(sum2)==0)
+		{
+			i++;
+			goto here;
+		}
+		else
+		{
+			final=final+i;
+			time++; 
+			printf("%d\n",i);
+		} 
+		} 
+	}
+	result=final/time;
+	printf("%.2f",result);
+} 
